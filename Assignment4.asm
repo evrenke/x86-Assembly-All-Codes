b0f535c94d125a27dcd29983df750a1234d7a5c4
evrenke <evrenkeskin300@gmail.com>
2019-02-12 14:05:36 -0800

    Update Assignment4.asm

--- Assignment4.asm
+++ Assignment4.asm
@@ -44,12 +44,17 @@ newArr  qword  5 dup  (?)
 ; add your data here
 introStr byte "You will need to enter 5 integers and 5 characters for two arrays. ", 0
 enterNumStr byte "Enter an integer: ", 0
-enterCharStr byte "Enter a character: ", 0
-numEntered dword ?
+numStr byte "The integer array you entered: ", 0
+enterCharStr byte "Enter 5 characters: ", 0
+charStr byte "The characters you entered: ", 0
+
 .code
 main proc
 	;----------------------------Your Code starts Here-----------------------
 	; 1. Prompt the user to enter 5 numbers then 5 letters
+	; 2. Save the numbers to a 32-bit integer array, numArr.
+	; The inputs are saved as they are taken from the user
+
 	mov edx, offset introStr
 	call writeString
 	call crlf
@@ -64,18 +69,23 @@ main proc
 		inc ebx
 	loop L1
 
+	; 1. Prompt the user to enter 5 numbers then 5 letters
+	; 3. Saves the letters to charArr where each element reserves one byte.
+	; Taking the character input from the user
+
+	mov edx, offset enterCharStr
+	call writeString
+
 	mov ecx, lengthof charArr
 	sub ebx, ebx
 	L2:
-		mov edx, offset enterCharStr
-		call writeString
 		call readChar
-		mov numArr[ebx], eax
+		mov charArr[ebx], al
 		inc ebx
 	loop L2
 	
-	;2. Saves the numbers to a 32-bit integer array, numArr.
-    ;3. Saves the letters to charArr where each element reserves one byte.
+	
+    
     ;4. Prints the numbers and letters. 
     ;5. prints the mean of the number array,numArr
     ;6. copy all the elements from the numArr and the charArr
